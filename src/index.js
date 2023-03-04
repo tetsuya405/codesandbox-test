34eabbf33a7addc24346d71d2591f057e4067625
tetsuya405 <ktm.htm.rtm.405@hotmail.co.jp>
2023-03-04 22:09:11 +0900

    lesson19 done

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
-import "./styles.css";
+// var val1 = "var変数"
+// console.log(val1)
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+
+// const val2= "let変数"
+// console.log(val2)
+
+// val2 = "let変数を上書き"
+// console.log(val2)
+
+// // let val2 = "再宣言"
+// console.log(val2)
+
+// const val4 = {
+//   name: "jake",
+//   age: 28,
+// }
+// val4.name = "change"
+// val4.address = "Tokyo"
+// console.log(val4)
+
+
+// const val5 = ['dog', 'cat'];
+// val5[0] = 'bird';
+// val5.push("monkey")
+// console.log(val5)

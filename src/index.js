0721c34d49282bf63ef68589ddd502fddafc3052
tetsuya405 <ktm.htm.rtm.405@hotmail.co.jp>
2023-03-04 22:41:43 +0900

    lesson 25 done

--- src/index.js
+++ src/index.js
@@ -99,5 +99,37 @@
  * lesson23 デフォルト値
  */
 
-const sayHello = (name = "guest") => console.log(`Hello, ${name}`);
-sayHello();
+// const sayHello = (name = "guest") => console.log(`Hello, ${name}`);
+// sayHello();
+
+/**
+ * lesson 24 スプレッド構文
+ */
+// 配列の展開
+// const arr1 = [1,2]
+// console.log(arr1)
+// console.log(...arr1)// 配列の中身を順番に処理する。
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1)
+
+
+// // まとめる
+// const arr2 = [1,2,3,4,5];
+// const [num1,num2,...arr3] = arr2;
+// console.log(num1)
+// console.log(num2)
+// console.log(arr3)
+
+// 配列のコピー
+const arr4 = [10,20];
+const arr5 = [30,40];
+
+const arr6 = [...arr4]
+
+console.log(arr6)
+const arr7 = [...arr4, ...arr5];
+console.log(arr7)
+
+
